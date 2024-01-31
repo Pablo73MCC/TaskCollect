4fe5bce1a1f0e46818b4273770b5dd2ad05e4d39
Pablo73MCC <pablo.franco@mccollect.mx>
2024-01-31 14:40:42 -0600

    ya jala
    ya me la jale

--- app/src/main/java/com/example/taskcollect/RecyclerClass_calendario.kt
+++ app/src/main/java/com/example/taskcollect/RecyclerClass_calendario.kt
@@ -2,10 +2,28 @@ package com.example.taskcollect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerClass_calendario : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recycler_class_calendario)
+class RecyclerClass_calendario(private val timeSlots: List<String>) :
+    RecyclerView.Adapter<RecyclerClass_calendario.TimeSlotViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeSlotViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_recycler_class_calendario, parent, false)
+        return TimeSlotViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TimeSlotViewHolder, position: Int) {
+        val timeSlot = timeSlots[position]
+        holder.timeTextView.text = timeSlot
+    }
+
+    override fun getItemCount() = timeSlots.size
+
+    class TimeSlotViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val timeTextView: TextView = itemView.findViewById(R.id.timeTextView)
     }
 }
